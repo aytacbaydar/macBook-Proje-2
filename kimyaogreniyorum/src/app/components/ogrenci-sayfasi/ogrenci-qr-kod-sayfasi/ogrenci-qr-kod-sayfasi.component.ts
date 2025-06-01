95ca6b5d608c6252a1d06ee823101288662a2c5a
Aytac <aytacbaydar@gmail.com>
2025-06-01 18:57:33 +0300

    01.06.2025-mac-18:57

---
+++ kimyaogreniyorum/src/app/components/ogrenci-sayfasi/ogrenci-qr-kod-sayfasi/ogrenci-qr-kod-sayfasi.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ogrenci-qr-kod-sayfasi',
+  standalone: false,
+  templateUrl: './ogrenci-qr-kod-sayfasi.component.html',
+  styleUrl: './ogrenci-qr-kod-sayfasi.component.scss'
+})
+export class OgrenciQrKodSayfasiComponent {
+
+}
--- kimyaogreniyorum/src/app/components/ogrenci-sayfasi/ogrenci-sayfasi.module.ts
+++ kimyaogreniyorum/src/app/components/ogrenci-sayfasi/ogrenci-sayfasi.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OgrenciQrKodSayfasiComponent } from './ogrenci-qr-kod-sayfasi/ogrenci-qr-kod-sayfasi.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OgrenciQrKodSayfasiComponent
+  ],
   imports: [
     CommonModule
   ]
