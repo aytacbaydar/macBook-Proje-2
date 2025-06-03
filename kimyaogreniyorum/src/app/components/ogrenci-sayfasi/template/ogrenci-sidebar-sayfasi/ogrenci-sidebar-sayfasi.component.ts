f257d5df97888eebfa0758ac89f77d6ae5723d65
Aytaç Baydar <60582171+aytacbaydar@users.noreply.github.com>
2025-06-03 21:19:35 +0300

    03.06.2025-dell-21:19

--- kimyaogreniyorum/src/app/components/ogrenci-sayfasi/template/ogrenci-sidebar-sayfasi/ogrenci-sidebar-sayfasi.component.ts
+++ kimyaogreniyorum/src/app/components/ogrenci-sayfasi/template/ogrenci-sidebar-sayfasi/ogrenci-sidebar-sayfasi.component.ts
@@ -10,15 +10,19 @@ export class OgrenciSidebarSayfasiComponent {
   isClosed = true;
 
   menuItems = [
-    { icon: 'dashboard', label: 'AnaSayfa', link: 'ogrenci-sayfasi' },
+    { icon: 'bi-house', label: 'AnaSayfa', link: 'ogrenci-sayfasi' },
     {
-      icon: 'qr_code_scanner',
+      icon: 'bi-qr-code-scan',
       label: 'QR Kod',
       link: 'ogrenci-sayfasi/ogrenci-qr-kod-sayfasi',
     },
-    { icon: 'description', label: 'Sayfalar' },
-    { icon: 'group', label: 'Kullanıcılar' },
-    { icon: 'exit_to_app', label: 'Çıkış' },
+    { icon: 'bi-camera-reels', label: 'Konu Anlatımı' },
+    { icon: 'bi-pencil-square', label: 'Sınavlar' },
+    { icon: 'bi-archive', label: 'Testler' },
+    { icon: 'bi-diagram-3', label: 'Yol Haritası' },
+    { icon: 'bi-graph-up-arrow', label: 'Konu Analizi' },
+    { icon: 'bi-filetype-pdf', label: 'İşlenen Konular' },
+    { icon: 'bi-credit-card-2-back', label: 'Ücretler' },
   ];
 
   toggleSidebar() {
